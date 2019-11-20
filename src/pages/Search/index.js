21a19f3fc32714cf460a791614463ac17172e1b2
Guilherme Welter <guilherme.a.welter@gmail.com.br>
2019-11-20 09:27:57 -0300

    Using .env and calling yelp api

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -2,20 +2,40 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 import SearchBar from "../../components/SearchBar";
+import yelp from "../../api/yelp";
 
 // import { Container } from './styles';
 
 export default function Search() {
   const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  async function searchApi() {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          location: "san jose",
+          term: search
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (error) {
+      console.log(error);
+      setErrorMessage("Something went wrong");
+    }
+  }
 
   return (
     <View>
       <SearchBar
         value={search}
         setValue={value => setSearch(value)}
-        inputAction={() => console.log("OPA")}
+        inputAction={searchApi}
       />
-      <Text>{search}</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>{results.length}</Text>
     </View>
   );
 }
